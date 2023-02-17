f663f69c78d405d574227cace94f05c3dfb7b47e
iyadaithou <66526110+iyadaithou@users.noreply.github.com>
2023-02-17 02:56:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,23 @@ class MyHTMLParser(HTMLParser):
         super().__init__()
         self.domain_url = domain_url
         self.urls = []
+        self.data = ''
         
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if attr[0] == "href" and self.domain_url in attr[1]:
                 self.urls.append(attr[1])
                 
+    def handle_data(self, data):
+        self.data += data
+                
     def get_urls(self):
         return self.urls
+    
+    def get_data(self):
+        return self.data
 
-def get_all_pages(domain_url, loop):
+def get_all_pages(domain_url):
     response = requests.get(domain_url)
     parser = MyHTMLParser(domain_url)
     parser.feed(response.text)
@@ -29,7 +36,7 @@ def get_all_pages(domain_url, loop):
     return page_urls
 
 
-async def process_page(url, loop):
+async def process_page(url):
     response = await loop.run_in_executor(None, requests.get, url)
     parser = MyHTMLParser(url)
     parser.feed(response.text)
@@ -46,25 +53,25 @@ async def process_page(url, loop):
     return relevant_info
 
 
-async def process_pages(urls, loop):
+async def process_pages(urls):
     tasks = []
     for url in urls:
-        tasks.append(asyncio.ensure_future(process_page(url, loop)))
+        tasks.append(asyncio.ensure_future(process_page(url)))
     results = await asyncio.gather(*tasks)
     return ' '.join(results)
 
 
 def main():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     input_text = st.text_input("Paste the website url",
                                placeholder="Paste the opportunity's url, and let us do the magic!")
     if not input_text:
         return
 
     with st.spinner("Fetching and summarizing content. Please wait..."):
-        list_of_urls = get_all_pages(str(input_text), loop)
-        relevant_info = loop.run_until_complete(process_pages(list_of_urls, loop))
+        list_of_urls = get_all_pages(str(input_text))
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        relevant_info = loop.run_until_complete(process_pages(list_of_urls))
         prompt = "Avoid repetition and being too generic, and write in a clear style with an advising tone. First define what the website is about, then list the qualities or strengths I should focus on to be a good fit for this opportunity and get accepted, follow that by stories and examples of how to talk about those qualities. Also, please write a couple of paragraphs analyzing what they might be looking for. Make sure to refer to this as a program. Here is the text: " + relevant_info
         response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=2000)
         brainstorming_output = response['choices'][0]['text']
