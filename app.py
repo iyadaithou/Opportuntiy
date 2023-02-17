8aa786e3e55a641da7c8d29600381bce4e64239a
iyadaithou <66526110+iyadaithou@users.noreply.github.com>
2023-02-17 03:32:54 -0500

    a

--- app.py
+++ app.py
@@ -2,80 +2,74 @@ import streamlit as st
 import openai
 import requests
 from html.parser import HTMLParser
-from multiprocessing import Process, Queue
+from multiprocessing import Pool
 
 st.set_page_config(page_title="Brainlyne Opportunity Analyzer")
 openai.api_key = st.secrets["openaiKey"]
+
 
 class MyHTMLParser(HTMLParser):
     def __init__(self, domain_url):
         super().__init__()
         self.domain_url = domain_url
         self.urls = []
-        
+
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if attr[0] == "href" and self.domain_url in attr[1]:
                 self.urls.append(attr[1])
-                
+
     def get_urls(self):
         return self.urls
 
-def get_all_pages(domain_url):
-    response = requests.get(domain_url)
-    parser = MyHTMLParser(domain_url)
-    parser.feed(response.text)
-    page_urls = parser.get_urls()
-    return page_urls
 
-
-def process_page(url, queue):
+def process_page(url,relevant_info):
     response = requests.get(url)
     parser = MyHTMLParser(url)
     parser.feed(response.text)
-    text_content = '\n'.join(parser.get_data())
+    text_content = "\n".join(parser.get_data())
     summary = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"What does this organization do? Which list of qualities will someone need to have to work here, get accepted, or join the team? Make it concise and very brief without losing relevant information. {text_content}",
+        prompt=f"How can you use the "+text_content+" to improve the response "+ relevant_info+" and give a more accurate organization to what does this organization do and which qaulities does an applicant or person need to join them or be selected?"
         temperature=0.2,
-        max_tokens=100,
+        max_tokens=450,
         n=1,
         stop=None,
     )
-    relevant_info = summary['choices'][0]['text']
-    queue.put(relevant_info)
+    relevant_info = summary["choices"][0]["text"]
+    return relevant_info
 
 
 def process_pages(urls):
-    queue = Queue()
-    processes = []
+    relevant_info = ""
     for url in urls:
-        p = Process(target=process_page, args=(url, queue))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
-
-    results = []
-    while not queue.empty():
-        results.append(queue.get())
-
-    return ' '.join(results)
+        new_relevant_info = process_page(url,relevant_info)
+        if new_relevant_info:
+            relevant_info = new_relevant_info
+    return relevant_info
 
 
 def main():
-    input_text = st.text_input("Paste the website url",
-                               placeholder="Paste the opportunity's url, and let us do the magic!")
+    input_text = st.text_input(
+        "Paste the website url",
+        placeholder="Paste the opportunity's url, and let us do the magic!",
+    )
     if not input_text:
         return
 
     with st.spinner("Fetching and summarizing content. Please wait..."):
-        list_of_urls = get_all_pages(str(input_text))
-        relevant_info = process_pages(list_of_urls)
-        prompt = "Avoid repetition and being too generic, and write in a clear style with an advising tone. First define what the website is about, then list the qualities or strengths I should focus on to be a good fit for this opportunity and get accepted, follow that by stories and examples of how to talk about those qualities. Also, please write a couple of paragraphs analyzing what they might be looking for. Make sure to refer to this as a program. Here is the text: " + relevant_info
-        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=2000)
-        brainstorming_output = relevant_info
+        list_of_urls = MyHTMLParser(input_text).get_urls()
+        relevant_info = process_pages(list_of_urls[:5])
+        prompt = (
+            "Avoid repetition and being too generic, and write in a clear style with an advising tone. First define what the website is about, then list the qualities or strengths I should focus on to be a good fit for this opportunity and get accepted, follow that by stories and examples of how to talk about those qualities. Also, please write a couple of paragraphs analyzing what they might be looking for. Make sure to refer to this as a program. Here is the text: "
+            + relevant_info
+        )
+        response = openai.Completion.create(
+            engine="text-davinci-003", prompt=prompt, max_tokens=2000
+        )
+        brainstorming_output = response["choices"][0]["text"]
         st.info(brainstorming_output)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
