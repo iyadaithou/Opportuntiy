669a2c19be218dc94d06ba1286bbd433adc1b29f
iyadaithou <66526110+iyadaithou@users.noreply.github.com>
2023-02-17 02:58:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 import requests
 from html.parser import HTMLParser
-import asyncio
+from multiprocessing import Process, Queue
 
 st.set_page_config(page_title="Brainlyne Opportunity Analyzer")
 openai.api_key = st.secrets["openaiKey"]
@@ -12,21 +12,14 @@ class MyHTMLParser(HTMLParser):
         super().__init__()
         self.domain_url = domain_url
         self.urls = []
-        self.data = ''
         
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if attr[0] == "href" and self.domain_url in attr[1]:
                 self.urls.append(attr[1])
                 
-    def handle_data(self, data):
-        self.data += data
-                
     def get_urls(self):
         return self.urls
-    
-    def get_data(self):
-        return self.data
 
 def get_all_pages(domain_url):
     response = requests.get(domain_url)
@@ -36,12 +29,12 @@ def get_all_pages(domain_url):
     return page_urls
 
 
-async def process_page(url):
-    response = await loop.run_in_executor(None, requests.get, url)
+def process_page(url, queue):
+    response = requests.get(url)
     parser = MyHTMLParser(url)
     parser.feed(response.text)
-    text_content = parser.get_data()
-    summary = await openai.Completion.create(
+    text_content = '\n'.join(parser.get_data())
+    summary = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"What does this organization do? Which list of qualities will someone need to have to work here, get accepted, or join the team? Make it concise and very brief without losing relevant information. {text_content}",
         temperature=0.2,
@@ -50,14 +43,24 @@ async def process_page(url):
         stop=None,
     )
     relevant_info = summary['choices'][0]['text']
-    return relevant_info
+    queue.put(relevant_info)
 
 
-async def process_pages(urls):
-    tasks = []
+def process_pages(urls):
+    queue = Queue()
+    processes = []
     for url in urls:
-        tasks.append(asyncio.ensure_future(process_page(url)))
-    results = await asyncio.gather(*tasks)
+        p = Process(target=process_page, args=(url, queue))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    results = []
+    while not queue.empty():
+        results.append(queue.get())
+
     return ' '.join(results)
 
 
@@ -69,14 +72,7 @@ def main():
 
     with st.spinner("Fetching and summarizing content. Please wait..."):
         list_of_urls = get_all_pages(str(input_text))
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        relevant_info = loop.run_until_complete(process_pages(list_of_urls))
+        relevant_info = process_pages(list_of_urls)
         prompt = "Avoid repetition and being too generic, and write in a clear style with an advising tone. First define what the website is about, then list the qualities or strengths I should focus on to be a good fit for this opportunity and get accepted, follow that by stories and examples of how to talk about those qualities. Also, please write a couple of paragraphs analyzing what they might be looking for. Make sure to refer to this as a program. Here is the text: " + relevant_info
         response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=2000)
-        brainstorming_output = response['choices'][0]['text']
-        st.info(brainstorming_output)
-
-
-if __name__ == '__main__':
-    main()
+        brainstorm
